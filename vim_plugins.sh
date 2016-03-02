225da3694af11727b4dd144a52db079bbe0d6e18
Yuanchao Sun <yuanchao.sun@gmail.com>
2016-03-02 19:30:02 +0800

    base config

--- vim_plugins.sh
+++ vim_plugins.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+cp HOME/.gitconfig ~/
+cp HOME/.vimrc ~/
+
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
